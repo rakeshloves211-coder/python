45f79753533d4953d1f8a2c8852b131e9d176a1f
Srinath Reddy <srinathreddy@rakesh-macbook.local>
2026-03-12 11:49:20 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,106 @@
+#TASK1 :
+print("Hello World", end = " ")
+print("Welocome Python")
+
+
+print ("Laptop","Mouse","Keyboard", sep = " | ")
+
+
+
+#TASK2 :
+name = "Ravi"
+age = "22"
+city = "Chennai"
+print(name , age , city , sep = "-")
+
+
+
+
+#TASK3 :
+name, age, student = "Meena", 20, "True"
+print("Profile = ", name, age, student)
+
+
+
+#TASK4=
+word = "Python"
+print("First letter= ",word[0] )
+print("third letter= ", word[4])
+print("Last letter= ", word[5])
+
+
+
+#TASK 5: ARTHEMATIC OPERATIONS
+print(25 + 10)
+print(50 - 20)
+print(8 * 5)
+print(100 / 10)
+print(10 % 3)
+print(2 ** 4)
+print(20 // 3)
+
+
+#TASK 6 - BODMAS Expression 
+
+print(3 + 2 * 5 ** 2) 
+
+
+#TASK 7 - Assignment Operator
+num = 50
+num += 25
+print(num)
+
+num = 100
+num /= 10
+print(num)
+
+#TASK 8 COMPARISON OPERATORS
+
+print(10 > 5)
+print(20 < 15)
+print(5 == 5)
+print(10 != 8)
+print(7 >= 7)
+print(6 <= 2)
+
+
+#TASK sTRTING cOMPARISON 
+
+a = "apple"
+b = "Apple"
+
+print(a == b)
+
+#TASK 10 LOGICAL OPERATORS
+
+print(10 > 5 and 5 == 5)
+print(5 > 10 or 10 == 10)
+print(not(5 > 2))
+
+
+#TASK 11 MEMBERSHIP OPERATOR
+numbers = [10, 20, 30, 40, 50]
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+
+#TASK 12 SWAP VARIABLES
+
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print(a , b)
+
+
+# TASK 13 BITWISE XOR
+
+a = 6
+b = 3
+print(a ^ b)
+
+
+
+
