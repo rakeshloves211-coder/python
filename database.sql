3dbc575133ae79899fb71f264fcf37347b2b070a
Srinath Reddy <srinathreddy@rakesh-macbook.local>
2026-04-09 12:54:39 +0530

    completed SQL

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE expenses_db;
+ 
+USE expenses_db;
+ 
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+ 
+CREATE TABLE expenses (
+    exp_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    amount FLOAT,
+    category VARCHAR(50),
+    description VARCHAR(100),
+    date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
